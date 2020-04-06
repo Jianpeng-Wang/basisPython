0d0faca36d8e49ee10d9131abd3ed1d6209153f8
Jianpeng-Wang <1015487391@qq.com>
2020-04-06 21:07:12 +0800

    元组只有查找操作

---
+++ 38_元组常见操作.py
@@ -0,0 +1,22 @@
+t1 = ('aa', 'bb', 'cc', 'bb')
+
+# 1. 下标
+# print(t1[0])
+
+# 2. index()
+# print(t1.index('bb'))  # 1
+# print(t1.index('bbb'))  # 保错
+
+# 3. count()
+# print(t1.count('aa'))
+# print(t1.count('aaa'))
+
+# 4. len()
+print(len(t1))
+
+
+
+
+
+
+
